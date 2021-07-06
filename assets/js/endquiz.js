1c18e4f6b98ed029c8cb226c1ce88e095f2ee934
Kristina Borisova Hadzhieva <kristinahadzhieva111@gmail.com>
2021-07-06 13:47:39 +0000

    Add JavaScript to end quiz page to make it functional

---
+++ assets/js/endquiz.js
@@ -0,0 +1,29 @@
+// -- End Game Page Script -- //
+//  Variables 
+const username = document.querySelector('#username');
+const saveScore = document.querySelector('#save-high-score');
+const finalScore = document.querySelector('#game-final-score');
+const recentScore = localStorage.getItem('recentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+finalScore.innerText = recentScore;
+// User needs to type their name into the input container to remove the disabled option on the save button. //
+username.addEventListener('keyup', () => {
+   saveScore.disabled = !username.value;
+});
+/*  When save button is clicked, save the user name and score. */
+let saveHighScore = e => {
+    e.preventDefault();
+
+    const score = {
+        score: recentScore,
+        name: username.value
+    };
+
+    highScores.push(score);
+};
+
+function Redirect(pageName) {
+    window.location.href = pageName;
+};
